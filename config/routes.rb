22b0eaebb310d769695c4d2cd8c58aca3cec51b1
William Rogers <williamr@gmail.com>
2014-12-29 01:50:35 -0500

    have a basic template working; need to set up authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :gifts
+
   devise_for :users
   #get 'pages/home'
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141229042842) do
+ActiveRecord::Schema.define(version: 20141229063036) do
+
+  create_table "gifts", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  add_index "gifts", ["user_id"], name: "index_gifts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
