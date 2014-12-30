2173ce847eac1a105ea51f4c65a6a04729a76313
William Rogers <williamr@gmail.com>
2014-12-29 22:56:39 -0500

    fixing error in paperclip on heroku

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -2,4 +2,6 @@ class Gift < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :item_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	has_attached_file :wrapped_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	validates_attachment_content_type :item_image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	validates_attachment_content_type :wrapped_image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
