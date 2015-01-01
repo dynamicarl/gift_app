08059aeb2ce46147e9e67188511abc589140a5dc
William Rogers <williamr@gmail.com>
2015-01-01 15:09:53 -0500

    finalized layout of the recipients list

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -4,4 +4,6 @@ class Gift < ActiveRecord::Base
 	has_attached_file :wrapped_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :item_image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	validates_attachment_content_type :wrapped_image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	
+	
 end
